89b00923ae8ee109f6781e41e7d1fdf1f016e46f
quyi-ai <djjdkdj829@gmail.com>
2026-05-02 22:39:57 +0800

    .

--- hwx/main.py
+++ hwx/main.py
@@ -4,8 +4,7 @@ SPEED = 0.5
 START_TIME = 480.0
 TIME_LIMIT = 30.0
 REWARD_PER_ORDER = 10
-def parse_order(line):
-
+def parse_order(line):#处理订单，返回订单列表
     parts = line.split()
     order = {
         "id": int(parts[0]),
@@ -19,7 +18,7 @@ def parse_order(line):
     return order
 
 
-def parse_input(text):
+def parse_input(text):#处理输入，返回字典
 
     lines = []
 
@@ -55,17 +54,17 @@ def parse_input(text):
         "orders": orders,
     }
 
-def create_couriers(courier_count):
+def create_couriers(courier_count):#创建外卖员，返回列表
     couriers=[]
     for i in range(courier_count):
         couriers.append({'id':i+1,'x':0,'y':0,'available_time':START_TIME})
     return couriers
 
 
-def manhattan_distance(x1,y1,x2,y2):
+def manhattan_distance(x1,y1,x2,y2):#计算距离
     return abs(x1-x2)+abs(y1-y2)
 
-def calc_preorder_delivery(courier, order):
+def calc_preorder_delivery(order, courier):#计算外卖员送预订单时间
     x,y=courier['x'],courier['y']
     sx,sy=order['sx'],order['sy']
     ex,ey=order['ex'],order['ey']
@@ -78,3 +77,68 @@ def calc_preorder_delivery(courier, order):
     time_to_delivery=dis_delivery/SPEED
     delivery_time=time_to_delivery+pick_time
     return delivery_time
+
+def calc_instant_delivery(order,courier):
+    x,y=courier['x'],courier['y']
+    sx,sy=order['sx'],order['sy']
+    ex,ey=order['ex'],order['ey']
+
+    dis_to_pick=manhattan_distance(x,y,sx,sy)
+    time_to_pick=dis_to_pick/SPEED
+    start_time = max(courier['available_time'], order['t'])
+    pick_time = start_time + time_to_pick
+    dis_delivery=manhattan_distance(sx,sy,ex,ey)
+    time_to_delivery=dis_delivery/SPEED
+    delivery_time=time_to_delivery+pick_time
+    return delivery_time
+    
+def is_success(order,delivery_time):#判断按时送达
+    if delivery_time<=TIME_LIMIT+order['t']:
+        return True
+    else:
+        return False
+def choose_best_courier(order,couriers):#选择合适的外卖员，返回外卖员和送达时间
+    best_courier = None
+    best_delivery_time = None
+    for courier in couriers:
+        if order['type']=='pre-order':
+            delivery_time=calc_preorder_delivery(order,courier)
+        else:
+            delivery_time=calc_instant_delivery(order,courier)
+        if is_success(order,delivery_time):
+            if best_courier==None or (best_delivery_time>delivery_time):
+                best_courier=courier
+                best_delivery_time=delivery_time
+    return best_courier,best_delivery_time
+def assign_order(order,courier,delivery_time):#修改外卖员状态
+    courier['available_time']=delivery_time
+    courier['x']=order['ex']
+    courier['y']=order['ey']
+
+def schedule_orders(orders,couriers):
+    results=[]
+    total_completed=0
+    for order in orders:
+        best_courier,best_delivery_time=choose_best_courier(order,couriers)
+        if best_courier==None:
+            results.append(
+                {
+                "id": order["id"],
+                "courier_id": 0,
+                "delivery_time": -1.0,
+                "success": 0,
+                }
+            )
+        else:
+            assign_order(order,best_courier,best_delivery_time)
+            results.append(
+                {
+                    'id':order['id'],
+                    'courier_id':best_courier['id'],
+                    'delivery_time':best_delivery_time,
+                    'success':1
+                }
+            )
+            total_completed+=1
+    total_revenue=total_completed*REWARD_PER_ORDER
+    return results,total_completed,total_revenue
